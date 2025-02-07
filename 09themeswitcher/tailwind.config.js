5bea6114ace8cdce6198f72236745b645900665e
ayushr12 <codehelp13@gmail.com>
2025-02-08 01:27:51 +0530

    ThemeChanger

---
+++ 09themeswitcher/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    themeMode: "class", // Enables class-based dark mode
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
